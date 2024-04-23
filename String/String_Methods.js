b2f0a68a27441ba626898ed3470ffb3c4f8a64ae
shaikhfaizan <faizanshaikh2002320@gmail.com>
2024-04-23 19:41:51 +0530

    Remove String folder

--- String/String_Methods.js
+++
@@ -1,4 +0,0 @@
-// var str = "Faizan";
-// // chartAt, concat, endsWith, includes, indexOf, lastIndexOf, match, padStart, padEnd, repeat, replace, search, slice, split, substring, toLowerCase, toUpperCase, trim, trimStart, trimEnd
-
-// console.log(str.trim());
