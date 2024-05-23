f58769e2b301a0ad2a46e158b17cb731b8789a0a
shaikhfaizan <faizanshaikh2002320@gmail.com>
2024-05-23 23:54:55 +0530

    Added proper alingments

--- recursion.js
+++ recursion.js
@@ -3,9 +3,8 @@
 // Functional that returns result.
 // Parametrized that generally don't return anything.
 
-// ================ Problem 1 Start ================
+// ================ Problem 1 Start ================================
 // Count Digits using Recursion.
-
 function digit(n, count = 0) {
 	if (n === 0) {
 		return count;
@@ -15,9 +14,11 @@ function digit(n, count = 0) {
 }
 
 // console.log(digit(12345));
-// ================ Problem 1 End ================
+// ================ Problem 1 End ================================
 
-// ================ Problem 2 Start ================
+
+
+// ================ Problem 2 Start ================================
 // Sum of Digits using Recursion.
 function helper(n, sum = 0) {
 	if (n === 0) {
@@ -36,11 +37,12 @@ function sumOfDigits(n) {
 }
 // let res = sumOfDigits(12345);
 // console.log(res);
-// ================ Problem 2 End ================
+// ================ Problem 2 End ================================
 
-// ================ Problem 3 start ================
+
+
+// ================ Problem 3 start ================================
 // Reverse a number using Recursion.
-
 function reversedFunction(n, rev = 0) {
 	if (n === 0) {
 		return rev;
@@ -53,9 +55,11 @@ function reversedFunction(n, rev = 0) {
 function reverseANumber(n) {
 	return reversedFunction(n);
 }
-// ================ Problem 3 End ================
+// ================ Problem 3 End ================================
 
-// ================ Problem 4 start ================
+
+
+// ================ Problem 4 start ================================
 function reverseString(str, rev = "", index = str.length - 1) {
 	if (index === 0) {
 		rev = rev + str[index];
@@ -70,9 +74,11 @@ function stringReverse(str) {
 	return reverseString(str);
 }
 // console.log(stringReverse("faizan"));
-// ================ Problem 4 End ================
+// ================ Problem 4 End ================================
 
-// ================ Problem 5 start ================
+
+
+// ================ Problem 5 start ================================
 // Print 1 to N using Recursion
 function printN(n, index = 1) {
 	if (index === n) {
@@ -85,9 +91,11 @@ function printN(n, index = 1) {
 }
 
 // printN(100);
-// ================ Problem 5 End ================
+// ================ Problem 5 End ================================
 
-// ================ Problem 6 start ================
+
+
+// ================ Problem 6 start ================================
 function printSumOfN(n, sum = 0, index = 1) {
 	if (n === index) {
 		return sum;
@@ -96,9 +104,11 @@ function printSumOfN(n, sum = 0, index = 1) {
 	return printSumOfN(n, sum, index + 1);
 }
 // console.log(printSumOfN(10));
-// ================ Problem 6 End ================
+// ================ Problem 6 End ================================
 
-// ================ Problem 7 start ================
+
+
+// ================ Problem 7 start ================================
 function factorial(n, fact = 1, index = 1) {
 	if (index === n) {
 		fact = fact * index;
@@ -110,9 +120,11 @@ function factorial(n, fact = 1, index = 1) {
 }
 
 // console.log(factorial(4));
-// ================ Problem 7 End ================
+// ================ Problem 7 End ================================
 
-// ================ Problem 8 start ================
+
+
+// ================ Problem 8 start ================================
 // Count the zeros
 function countZeros(n, count = 0) {
 	if (n === 0) {
@@ -124,11 +136,12 @@ function countZeros(n, count = 0) {
 	n = Math.floor(n / 10);
 	return countZeros(n, count);
 }
-
 // console.log(countZeros(1003402));
-// ================ Problem 8 End ================
+// ================ Problem 8 End ================================
 
-// ================ Problem 9 start ================
+
+
+// ================ Problem 9 start ================================
 // Palindrome
 function palindrome(n, org = n, rev = 0) {
 	if (n === 0) {
@@ -148,9 +161,11 @@ function palindrome(n, org = n, rev = 0) {
 
 // let result = palindrome(1001);
 // console.log(result ? "It is Palindrome" : "It is not Palindrome");
-// ================ Problem 9 End ================
+// ================ Problem 9 End ================================
 
-// ================ Problem 10 start ================
+
+
+// ================ Problem 10 start ================================
 // Iterate over an Array using Recursion
 function iterateArray(arr, index = 0) {
 	if (arr.length === index) {
@@ -162,9 +177,11 @@ function iterateArray(arr, index = 0) {
 	iterateArray(arr, index + 1);
 }
 // iterateArray([1, 2, 3, 4, 5]);
-// ================ Problem 10 End ================
+// ================ Problem 10 End ================================
 
-// ================ Problem 11 start ================
+
+
+// ================ Problem 11 start ================================
 // Print N to 1
 function printN(n) {
 	if (n < 1) {
@@ -178,7 +195,9 @@ function printN(n) {
 // printN(5);
 // ================ Problem 11 End ================
 
-// ================ Problem 12 start ================
+
+
+// ================ Problem 12 start ================================
 // Sum of N numbers
 function sumOfN(n, total = 0) {
 	if (n < 1) {
@@ -189,9 +208,11 @@ function sumOfN(n, total = 0) {
 	return sumOfN(n - 1, total);
 }
 // console.log(sumOfN(5));
-// ================ Problem 12 End ================
+// ================ Problem 12 End ================================
 
-// ================ Problem 13 start ================
+
+
+// ================ Problem 13 start ================================
 // using recursion functional approach
 function sumOfN(n) {
 	if (n === 0) {
@@ -201,9 +222,11 @@ function sumOfN(n) {
 	return n + sumOfN(n - 1);
 }
 // console.log(sumOfN(5));
-// ================ Problem 13 End ================
+// ================ Problem 13 End ================================
 
-// ================ Problem 14 start ================
+
+
+// ================ Problem 14 start ================================
 // product of N using functional approach
 function productOfN(n) {
 	if (n > 3) {
@@ -214,9 +237,11 @@ function productOfN(n) {
 }
 
 // console.log(productOfN(1));
-// ================ Problem 14 End ================
+// ================ Problem 14 End ================================
 
-// ================ Problem 15 start ================
+
+
+// ================ Problem 15 start ================================
 // sum of Array element
 function sumOfArray(arr) {
 	if (arr.length === 0) {
@@ -227,9 +252,11 @@ function sumOfArray(arr) {
 	return arr[arr.length - 1] + sumOfArray(arr.slice(0, arr.length - 1));
 }
 // console.log(sumOfArray([1, 2, 3, 4]));
-// ================ Problem 15 End ================
+// ================ Problem 15 End ================================
 
-// ================ Problem 16 start ================
+
+
+// ================ Problem 16 start ================================
 // count number of digits
 function countNumberOfDigits(n, count = 0) {
 	if (n === 0) {
@@ -240,9 +267,11 @@ function countNumberOfDigits(n, count = 0) {
 	return countNumberOfDigits(Math.floor(n / 10), count);
 }
 // console.log(countNumberOfDigits(123455));
-// ================ Problem 16 End ================
+// ================ Problem 16 End ================================
 
-// ================ Problem 17 start ================
+
+
+// ================ Problem 17 start ================================
 // Approach = 1
 // reverse an array using the functional recursion.
 function reverseAnArray(arr, rev = []) {
@@ -272,9 +301,11 @@ function reverse(arr, index = 0) {
 	return reverse(arr.slice(0, arr.length), index + 1);
 }
 // console.log(reverse([1, 2, 3, 4, 5]));
-// ================ Problem 17 End ================
+// ================ Problem 17 End ================================
 
-// ================ Problem 18 start ================
+
+
+// ================ Problem 18 start ================================
 function palindrome(str, index = 0) {
 	if (index === Math.floor(str.length / 2)) {
 		return true;
@@ -287,9 +318,9 @@ function palindrome(str, index = 0) {
 }
 
 // console.log(palindrome("madqm"));
-// ================ Problem 18 End ================
+// ================ Problem 18 End ================================
 
-// ================ Problem 19 start ================
+// ================ Problem 19 start ================================
 // Fibonacci
 // Not optimized time complexity O(n^2)
 function fib(n) {
@@ -303,9 +334,11 @@ function fib(n) {
 	return last + second_last;
 }
 // console.log(fib(4));
-// ================ Problem 19 End ================
+// ================ Problem 19 End ================================
 
-// ================ Problem 20 start ================
+
+
+// ================ Problem 20 start ================================
 // Fibonacci
 function fib(n, arr = [0, 1], index = 2) {
 	if (index === n) {
@@ -316,9 +349,11 @@ function fib(n, arr = [0, 1], index = 2) {
 }
 // let result = fib(10);
 // console.log(result.pop());
-// ================ Problem 20 End ================
+// ================ Problem 20 End ================================
 
-// ================ Problem 21 start ================
+
+
+// ================ Problem 21 start ================================
 // Fibonacci optimal version
 // In this I tract the calculation, not to repeat the same calculation often.
 function fib(n, calculated = {}) {
@@ -334,11 +369,12 @@ function fib(n, calculated = {}) {
 }
 
 // console.log(fib(10));
-// ================ Problem 21 End ================
+// ================ Problem 21 End ================================
+
+
 
 // ================ Problem 22 start ==============================================
 // Sum, Product, Count, Reverse, occurrences, checkZeros, removeDup, largest, smallest, second_largest, kth, peakElement of an Array elements using Recursion.
-
 function operations(
 	// Each time the references will be going to provided for each function calls so each time a function gets calls a new function executions context created that has its own copies of all this parameters a updated recursive values of parameters.
 	arr,
