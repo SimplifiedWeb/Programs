17fc3aab3451dad3259f687a6819dc3e20e1c7d2
shaikhfaizan <faizanshaikh2002320@gmail.com>
2024-06-05 15:56:56 +0530

    random programs

---
+++ miscellaneous_programs.js
@@ -0,0 +1,29 @@
+// ================ Problem 1 Start ================================
+// Given a string array words, return an array of all characters that show up in all strings within the words (including duplicates). You may return the answer in any order
+function generateWords(str1, str2) {
+	let result = [];
+	for (let i = 0; i < str1.length; i++) {
+		currElm = str1[i];
+		for (let j = 0; j < str2.length; j++) {
+			if (currElm === str2[j]) {
+				result.push(currElm);
+			}
+		}
+	}
+	return result;
+}
+
+function findWords(arr) {
+	if (arr.length < 1) return "Must be atleast two";
+	let currElm = generateWords(arr[0], arr[1]);
+
+	for (let i = 2; i < arr.length; i++) {
+		currElm = generateWords(arr[i], currElm);
+	}
+
+	return [...new Set(currElm)].sort();
+}
+// let result = findWords(["bella", "label", "roller"]);
+// console.log(result);
+
+// ================ Problem 1 End ================================
